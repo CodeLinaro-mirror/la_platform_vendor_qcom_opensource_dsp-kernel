bce99cb8782bf8b6ac8a33816dd5ac60585d7e18
Anirudh Raghavendra <quic_araghave@quicinc.com>
2025-05-21 16:33:34 -0700

    dsp-kernel: Fix bug when freeing extended context
    When the user object of client was being freed, the usecount of extended
    context was not being decremented. This would result in extended mapping
    failing when being run back to back. Fix this bug by decrementing the
    usecount of extended context.
    
    Change-Id: I9d30c5f48db2ac1599f0bec2e8fe9af6b49e6574
    Signed-off-by: Anirudh Raghavendra <quic_araghave@quicinc.com>

--- dsp/fastrpc.c
+++ dsp/fastrpc.c
@@ -3876,6 +3876,9 @@ static int fastrpc_user_obj_free(struct fastrpc_user *user,
 		fastrpc_session_free(cctx, fl->sctx);
 	if (fl->secsctx)
 		fastrpc_session_free(cctx, fl->secsctx);
+	if (fl->extctx)
+		fastrpc_session_free(cctx, fl->extctx);
+
 	spin_lock_irqsave(&fl->dspsignals_lock, irq_flags);
 	for (i = 0; i < (FASTRPC_DSPSIGNAL_NUM_SIGNALS /FASTRPC_DSPSIGNAL_GROUP_SIZE); i++)
 		kfree(fl->signal_groups[i]);
