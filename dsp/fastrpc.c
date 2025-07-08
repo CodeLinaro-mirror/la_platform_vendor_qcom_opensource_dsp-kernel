648af3439fd5d7f15908546ddc3bb9e9e983d4db
ssakore <quic_ssakore@quicinc.com>
2025-07-08 19:45:14 +0530

    dsp-kernel: Do not update the status back to the default
    After a process is created on the DSP, if it fails to create an
    instance of the device due to an allocation failure at the kernel
    level, the process state is reset to the default state. This
    behavior can potentially be exploited by an attacker to trigger
    use-after-free vulnerabilities. To mitigate this risk, the process
    status should not be reset to the default state if the PD has been
    successfully created on the DSP.
    
    Change-Id: Ib2bdb1bb0c36c6fcca0898c97f97af87cf2b4fc8
    Signed-off-by: ssakore <quic_ssakore@quicinc.com>
    (cherry picked from commit 2030cf4d934621dbfd5097de1b28506600d1fb56)

--- dsp/fastrpc.c
+++ dsp/fastrpc.c
@@ -6400,13 +6400,8 @@ static long fastrpc_device_ioctl(struct file *file, unsigned int cmd,
 		break;
 	}
 
-	if (process_init && !err) {
+	if (process_init && !err)
 		err = fastrpc_device_create(fl);
-		if (err)
-			atomic_set(&fl->state, DEFAULT_PROC_STATE);
-		else
-			atomic_set(&fl->state, DSP_CREATE_COMPLETE);
-	}
 
 	spin_lock_irqsave(&cctx->lock, flags);
 	fastrpc_channel_update_invoke_cnt(cctx, false);
@@ -6813,6 +6808,7 @@ static int fastrpc_device_create(struct fastrpc_user *fl)
 	frpc_dev->fl = fl;
 	frpc_dev->handle = fl->tgid_frpc;
 	fl->device = frpc_dev;
+	atomic_set(&fl->state, DSP_CREATE_COMPLETE);
 	return err;
 }
 
