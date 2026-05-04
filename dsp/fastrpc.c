30cefd8eababc39bd63e59d6b7e59c1c3f770858
Adarsh Kumar Sinha <adarsinh@qti.qualcomm.com>
2026-05-04 15:38:44 +0530

    dsp-kernel: Fix memory leak for mdctx->phy_ids and mdctx->instance_ids
    In fastrpc_multidomain_ctx_setup and fastrpc_multidomain_ctx_cleanup, the fastrpc_mdctx_info mdctx is only partially freed, missing mdctx->phy_id and mdctx->instance_ids. The buffers phy_ids and instance_ids are always created in fastrpc_multidomain_ctx_obj_init and pointers mdctx->phy_ids and mdctx->instance_ids are assigned to the buffer.
    
    Fix partial cleanup and avoid memory leak by also freeing the alloacted buffers for phy_ids and instance_ids.
    
    Change-Id: I970a7d14130084dbda2e4f620b83e2bce385faf3
    Signed-off-by: Adarsh Kumar Sinha <adarsinh@qti.qualcomm.com>

--- dsp/fastrpc.c
+++ dsp/fastrpc.c
@@ -5708,6 +5708,8 @@ bail:
 		kfree(mdctx->tgids_frpc);
 		kfree(mdctx->session_ids);
 		kfree(mdctx->domains);
+		kfree(mdctx->phy_ids);
+		kfree(mdctx->instance_ids);
 		kfree(mdctx);
 	}
 	mutex_unlock(gmut);
@@ -5752,6 +5754,8 @@ static int fastrpc_multidomain_ctx_cleanup(struct fastrpc_user *fl,
 	kfree(mdctx->tgids_frpc);
 	kfree(mdctx->session_ids);
 	kfree(mdctx->domains);
+	kfree(mdctx->phy_ids);
+	kfree(mdctx->instance_ids);
 	kfree(mdctx);
 bail:
 	mutex_unlock(gmut);
